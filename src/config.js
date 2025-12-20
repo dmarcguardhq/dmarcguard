42d93a634846573775356623e4b555a2cfd1b215
Meysam Azad <meysam@meysam.io>
2025-12-21 00:30:43 +0700

    feat: override api base url in dev

--- src/config.js
+++ src/config.js
@@ -1,1 +1,8 @@
-export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || "/api";
+var BASE_API_URL = import.meta.env.VITE_BASE_API_URL || "/api";
+const isDev = import.meta.env.DEV;
+
+if (isDev) {
+  BASE_API_URL = "http://localhost:8080/api";
+}
+
+export var BASE_API_URL;
